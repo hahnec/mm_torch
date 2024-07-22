9701915877d6982f42ce08eb6414068d80951a70
hahnec <christopher.hahne@unibe.ch>
2024-07-22 15:46:19 +0200

    feat(dims): generalize function for multiple leading dimensions

--- mm_torch/functions/lu_chipman.py
+++ mm_torch/functions/lu_chipman.py
@@ -10,8 +10,8 @@ def lu_chipman(
         ):
 
     # init
-    h, w = M.shape[:2]
-    M = M.reshape(h, w, 4, 4)
+    hw = M.shape[:-2]
+    M = M.reshape(*hw, 4, 4)
     if transpose: M = M.transpose(-2, -1)
     if mask is None: mask = mm_filter(M)
 
@@ -23,11 +23,13 @@ def lu_chipman(
     U_R[mask], _, V_R[mask] = svd_fun(M_0[..., 1:4, 1:4][mask])
 
     # unit vector to replace rectangular diagonal matrix (capital sigma)
-    S_R = torch.eye(3, dtype=M.dtype, device=M.device)[None, None].repeat(h, w, 1, 1)
+    S_R = torch.eye(3, dtype=M.dtype, device=M.device)[None,]*len(hw)
+    S_R = S_R.repeat(*hw, 1, 1)
     S_R[..., -1, -1][torch.sign(torch.det(M)) < 0] = -1 # modification of MR when the determinant of M is negative
 
     # Construct MR
-    MR = torch.eye(4, dtype=M.dtype, device=M.device)[None, None].repeat(h, w, 1, 1)
+    MR = torch.eye(4, dtype=M.dtype, device=M.device)[None,]*len(hw)
+    MR = MR.repeat(*hw, 1, 1)
     MR[..., 1:4, 1:4] = U_R @ (S_R @ V_R)    #.transpose(-2, -1)
 
     # depolarization
@@ -47,18 +49,21 @@ def lu_chipman(
 
 def diattenuation_matrix(M):
 
-    h, w = M.shape[:2]
+    hw = M.shape[:-2]
 
     dvec = torch.stack([M[..., 0, 1], M[..., 0, 2], M[..., 0, 3]], dim=-1) / (M[..., 0, 0][..., None] +  1e-13)
     D = (M[..., 0, 1]**2 + M[..., 0, 2]**2 + M[..., 0, 3]**2)**.5
     D1 = (1 - D**2)**.5
     D, D1 = D[..., None, None], D1[..., None, None]
     
-    MD = torch.eye(4, dtype=M.dtype, device=M.device)[None, None].repeat(h, w, 1, 1)
+    MD = torch.eye(4, dtype=M.dtype, device=M.device)[None,]*len(hw)
+    MD = MD.repeat(*hw, 1, 1)
     MD[..., 0, 1:] = dvec
     MD[..., 1:, 0] = dvec
     outer_product = dvec[..., None] * dvec[..., None, :] # torch.outer(dvec, dvec)
-    MD[..., 1:, 1:] = D1 * torch.eye(3, device=M.device)[None, None].repeat(h, w, 1, 1) + (1 - D1) * outer_product / D**2
+    eye = torch.eye(3, device=M.device)[None,]*len(hw)
+    eye = eye.repeat(*hw, 1, 1)
+    MD[..., 1:, 1:] = D1 * eye + (1 - D1) * outer_product / D**2
     M_0 = M @ torch.linalg.inv(MD)
     MD[D.squeeze()==0] = torch.eye(4, dtype=M.dtype, device=M.device)
     M_0[D.squeeze()==0] = M[D.squeeze()==0]
