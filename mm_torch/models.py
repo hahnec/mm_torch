fix(norm): still use sliced matrices when there is no normalization

mm_torch/models.py
@@ -174,7 +174,7 @@ class MuellerMatrixSelector(nn.Module):
                 if self.norm_opt:
                     # concatenate normalized image
                     norm_img = x.sum(-1, keepdim=True)/x.flatten(1,-1).max()
-                    r = torch.cat([r, norm_img], dim=-1) if self.norm_opt else m
+                    r = torch.cat([r, norm_img], dim=-1) if self.norm_opt else r
                 else:
                     # merge 1,1 entry with 3x3 matrix
                     r = torch.cat((m[..., 0][..., None], r), dim=-1)